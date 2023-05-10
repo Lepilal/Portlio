c4506fea9ac76f1470c05d7236b179d4e8b501cb
gloria nashipae <glorialepilal@gmail.com>
2023-05-10 07:51:48 +0300

    front page

---
+++ scripts.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector('.hamburger');
+const navlink = document.querySelector('.nav__link');
+
+hamburger.addEventListener('click', () => (
+    navLink.classList.toggle('hide')
+));
+
